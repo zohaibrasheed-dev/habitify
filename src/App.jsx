a1fa8d9f9a0a4de737f542aba7c28b3d85284e02
zohaibrasheedcr7 <zohaibrasheedcr7@gmail.com>
2026-04-20 16:27:21 +0500

    redux-react

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import About from "./pages/About/About";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />} >
-        <Route index element={<Dashboard />} />
-        <Route path="/about" element={<About />} />
-      </Route>
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<MainLayout />} >
+          <Route index element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </Provider>
   )
 }
 
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,23 @@
+import styles from './About.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import { increment, decrement } from "../../redux/features/counterSlice";
+
 
 function About() {
+
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <div className="contentWrapper">
-            
+            <div className="counter">
+                <span className={styles.counter}>{count}</span>
+
+                <div className={`${styles.counterBtns} flex`}>
+                    <button type="button" onClick={() => dispatch(increment())} className="btn btn-primary">Increment</button>
+                    <button type="button" onClick={() => dispatch(decrement())} className="btn btn-primary">Decrement</button>
+                </div>
+            </div>
         </div>
     )
 }
